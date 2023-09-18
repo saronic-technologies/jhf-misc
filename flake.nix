ff7640debcb77c2f8d5ec334f86d26701df013a3
Jacob Freeman <jacob@saronic.com>
2023-09-18 11:18:27 -0500

    add flake for dev shell

---
+++ flake.nix
@@ -0,0 +1,13 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+    flake-utils.url = "github:numtide/flake-utils";
+    saronic.url = "git+ssh://git@github.com/saronic-technologies/prototype-software-merry";
+  };
+  outputs = { nixpkgs, saronic, flake-utils, ... }: flake-utils.lib.eachDefaultSystem (system:
+    rec {
+      devShell = saronic.devShells.${system}.zeddy-rays;
+    }
+  );
+}
+
